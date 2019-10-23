ddb990935fc50f7c7f2735367f675bfa6f32005b
Aida Abdikulova <abdikulova@gmail.com>
2019-10-23 05:02:20 +0000

    Automatically backed up by Learn

--- models/puppy.rb
+++ models/puppy.rb
@@ -2,12 +2,14 @@ class Puppy
 
   attr_accessor :name, :breed, :months_old
   @@all = []
+
   def initialize(name:,breed:,age:)
     @name = name
     @breed = breed
     @months_old = age
     @@all << self
   end
+
   def self.all
     @@all
   end
